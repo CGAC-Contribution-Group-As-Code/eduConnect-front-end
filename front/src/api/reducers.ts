79b967b4087bd1286706f695e151c9c7371cbd87
Jo MINSU <50093223+lookinmin@users.noreply.github.com>
2023-11-17 16:35:00 +0900

    로그인 기능 구현
    Redux를 통한 로그인 / 로그아웃 기능 구현

--- front/src/api/reducers.ts
+++ front/src/api/reducers.ts
@@ -1,4 +1,5 @@
 import { createSlice, configureStore, PayloadAction } from "@reduxjs/toolkit";
+import { useDispatch, useSelector } from "react-redux";
 
 interface UserState {
   id: string | null;
@@ -7,9 +8,10 @@ interface UserState {
 
 export const userSlice = createSlice({
   name: "user",
-  initialState: { id: null, role: 0 } as UserState,
+  initialState: { id: null, role: -1 } as UserState,
   reducers: {
     setUser: (state, action: PayloadAction<UserState>) => {
+      console.log(action.payload.id, action.payload.role, "inredux");
       state.id = action.payload.id;
       state.role = action.payload.role;
     },
